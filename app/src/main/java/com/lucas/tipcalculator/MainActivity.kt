5340a6e4d7c6f611522b593a7dfa24beae5e987e
almeiidalucaas <edson_elca15@hotmail.com>
2025-03-10 11:39:38 +0100

    Initial commit

---
+++ app/src/main/java/com/lucas/tipcalculator/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.lucas.tipcalculator
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    // Valor total da conta
+    // Numero de pessoas
+    // Porcentagem de gorjeta 10%, 15%, 20%
+    // Calcular
+    // Limpar espacos preenchidos
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
